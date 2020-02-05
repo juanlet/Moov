1d4543b07a51216d5cfdf846afb490b03036c0e7
Juan Letamendia <juanletamendia@gmail.com>
2020-02-05 17:56:58 -0300

    Changed some minor design issues

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -40,11 +40,11 @@ const Home = props => {
   }
 
   const filterMoviesByRating = () => {
-    const lowerBound = ratingFilter * 2 - 2;
-    const upperBound = ratingFilter * 2;  
+    const lowerBound = ratingFilter * 2;
+    const upperBound = ratingFilter * 2 + 2;  
     const filteredMovies =   movieList.filter(movie=>{
        const avgVote = parseInt(movie.vote_average);
-      if( avgVote > lowerBound && avgVote <=upperBound ){
+      if( avgVote >= lowerBound && avgVote <=upperBound ){
           return movie;
        }
         
