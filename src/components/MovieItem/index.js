0079ab330b713f80535f1e86eec48c5a2db0a54f
Juan Letamendia <juanletamendia@gmail.com>
2020-02-05 01:50:07 -0300

    Added layout improvements and media queries for mobile

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import './MovieItem.scss';
 
 function index(props) {
   return (
-    <div className="card">
+    <div className="card movie-card">
       <img
         src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}
 `}
+       className="movie-card__image"
         alt="Logo"
       />
       <div>{props.movie.title}</div>
-      <div>{props.movie.overview}</div>
+      {/* <div>{props.movie.overview}</div> */}
     </div>
   );
 }
