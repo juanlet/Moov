cd0c4223ccd940774df2ddab12e4fbee3d4dc3b9
Juan Letamendia <juanletamendia@gmail.com>
2020-02-05 04:34:42 -0300

    Added footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import  './Footer.scss';
 function index(props) {
-  return <div className="footer"></div>;
+  return <div className="footer">
+  <div className="input-group newsletter">
+        <span class="newsletter__subtitle">Subscribe to our newsletter</span>
+
+         <input type="text" className="form-control" placeholder="Email" aria-label="Search" aria-describedby="movie-search"/>
+         <div class="newsletter__subscribe-btn">Subscribe</div>
+  </div>
+  <div className="copyright">
+   © 2020 Moove
+  </div>
+  </div>;
 }
 
 index.propTypes = {};
